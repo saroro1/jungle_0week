a3118dd63dbaf8a6e763364bff8eca71fb5b6664
saroro <hbhj4633@naver.com>
2025-05-13 20:31:20 +0900

    로그아웃시 토큰 삭제 추가

--- main_router/html/auth.py
+++ main_router/html/auth.py
@@ -17,5 +17,5 @@ def sign_up_page():
 @auth_route.route("/sign_out")
 def sign_out():
     res = make_response(render_template("./auth/sign_out.html"))
-    res.set_cookie("Authorization", "", expires=0)
+    res.set_cookie("auth_token", "", expires=0)
     return res

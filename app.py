6a9a8b04bbcb8ba36f95ac020c326467151ac215
saroro <hbhj4633@naver.com>
2025-05-13 23:02:59 +0900

    인덱싱 자동으로 수정

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ load_dotenv()
 from flask import Flask, render_template
 
 import main_router
-from constant import DBContainer
+from constant import DBContainer, word_type
 
 app = Flask(__name__, static_url_path='/static')
 from pymongo import MongoClient, DESCENDING
@@ -19,17 +19,15 @@ app.register_blueprint(main_router.api.auth_api_router)
 app.register_blueprint(main_router.api.game_api_router)
 client = MongoClient('localhost', 27017)
 db = client['acid_rain']
-user_db = db['user']
-try:
-    user_db.create_index([("high_score.kr", DESCENDING)])
-except Exception as e:
-    print("인덱싱 생성 실패")
+user_db = db["user_db"]
+for i in range(word_type):
+    try:
+        user_db.create_index([(f"high_score.{i}", DESCENDING)])
+    except Exception as e:
+        print(f"인덱싱 생성 실패 {i}")
 
-# high_score.en 필드에 내림차순 인덱스 생성
-try:
-    user_db.create_index([("high_score.en", DESCENDING)])
-except Exception as e:
-    print("인덱싱 생성 실패")
+DBContainer.user_db = user_db
+
 
 @app.route("/", endpoint="page_main")
 @auth_middleware(use_redirect=True)
@@ -46,18 +44,22 @@ def get_acid_rain():  # put application's code here
 def post_acid_ranking():
     pass
 
+
 @app.route('/game')
 @auth_middleware(use_redirect=True)
 def main_game():
     return render_template("./game/acid_game.html")
 
+
 @app.route('/testgame')
 def test_game():
     return render_template("./game/acid_game.html")
 
+
 @app.route('/testranking')
 def test_rank():
     return render_template("./game/ranking.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True if os.environ.get('IS_DEBUG') else False, port=5000 if os.environ.get('IS_DEBUG') else 9001)
